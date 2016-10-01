cffabb9b3652e4de917a40f0bf7863d197af9b6d
Bennat P Jose <bennatjose@gmail.com>
2016-10-01 05:38:48 -0600

    Fixed API

---
+++ API2/API2/Controllers/tblCharitiesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API2.Models;
+
+namespace API2.Controllers
+{
+    public class tblCharitiesController : ApiController
+    {
+        private dbPerishableEntities db = new dbPerishableEntities();
+
+        // GET: api/tblCharities
+        public IQueryable<tblCharity> GettblCharities()
+        {
+            return db.tblCharities;
+        }
+
+        // GET: api/tblCharities/5
+        [ResponseType(typeof(tblCharity))]
+        public IHttpActionResult GettblCharity(int id)
+        {
+            tblCharity tblCharity = db.tblCharities.Find(id);
+            if (tblCharity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tblCharity);
+        }
+
+        // PUT: api/tblCharities/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PuttblCharity(int id, tblCharity tblCharity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tblCharity.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tblCharity).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tblCharityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/tblCharities
+        [ResponseType(typeof(tblCharity))]
+        public IHttpActionResult PosttblCharity(tblCharity tblCharity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.tblCharities.Add(tblCharity);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = tblCharity.Id }, tblCharity);
+        }
+
+        // DELETE: api/tblCharities/5
+        [ResponseType(typeof(tblCharity))]
+        public IHttpActionResult DeletetblCharity(int id)
+        {
+            tblCharity tblCharity = db.tblCharities.Find(id);
+            if (tblCharity == null)
+            {
+                return NotFound();
+            }
+
+            db.tblCharities.Remove(tblCharity);
+            db.SaveChanges();
+
+            return Ok(tblCharity);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tblCharityExists(int id)
+        {
+            return db.tblCharities.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ API2/API2/Controllers/tblOrderItemsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API2.Models;
+
+namespace API2.Controllers
+{
+    public class tblOrderItemsController : ApiController
+    {
+        private dbPerishableEntities db = new dbPerishableEntities();
+
+        // GET: api/tblOrderItems
+        public IQueryable<tblOrderItem> GettblOrderItems()
+        {
+            return db.tblOrderItems;
+        }
+
+        // GET: api/tblOrderItems/5
+        [ResponseType(typeof(tblOrderItem))]
+        public IHttpActionResult GettblOrderItem(int id)
+        {
+            tblOrderItem tblOrderItem = db.tblOrderItems.Find(id);
+            if (tblOrderItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tblOrderItem);
+        }
+
+        // PUT: api/tblOrderItems/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PuttblOrderItem(int id, tblOrderItem tblOrderItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tblOrderItem.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tblOrderItem).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tblOrderItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/tblOrderItems
+        [ResponseType(typeof(tblOrderItem))]
+        public IHttpActionResult PosttblOrderItem(tblOrderItem tblOrderItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.tblOrderItems.Add(tblOrderItem);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = tblOrderItem.Id }, tblOrderItem);
+        }
+
+        // DELETE: api/tblOrderItems/5
+        [ResponseType(typeof(tblOrderItem))]
+        public IHttpActionResult DeletetblOrderItem(int id)
+        {
+            tblOrderItem tblOrderItem = db.tblOrderItems.Find(id);
+            if (tblOrderItem == null)
+            {
+                return NotFound();
+            }
+
+            db.tblOrderItems.Remove(tblOrderItem);
+            db.SaveChanges();
+
+            return Ok(tblOrderItem);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tblOrderItemExists(int id)
+        {
+            return db.tblOrderItems.Count(e => e.Id == id) > 0;
+        }
+    }
+}
